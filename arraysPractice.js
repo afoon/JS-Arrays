39ddd8c7dbff1b94f239894ee21263c8ca4227b7
Aminata Foon <aminatafoon@Aminatas-MacBook-Air.local>
2017-08-29 12:22:08 -0500

    Array Practice

--- arraysPractice.js
+++ arraysPractice.js
@@ -3,7 +3,9 @@
 
 var arr = [10,20,30];
 //Create a function named 'first' that is given 'arr' as the argument and  returns the first item in the given array.
-
+function first (arr){
+  return arr[0];
+}
   //Code Here
 
 
@@ -16,6 +18,9 @@ var arr = [40,50,60];
 
 
   //Code Here
+function last(arr){
+  return arr[arr.length-1];
+}
 
 
 //Next Problem
@@ -25,6 +30,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 //Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
 
   //Code Here
+function looper (family){
+  for (var i =0; i < family.length; i++){
+    family[i];
+  }
+}
 
 
 //Next problem
@@ -35,6 +45,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 //Write a function called reversedLooper that is given letters as it's only argument and loops through the given array backwards alerting every item in the array starting at the end.
 
   //Code Here
+function reversedLooper(letters){
+  for (var i = letters.length -1; i < letters.length; i++){
+    letters[i];
+  }
+}
 
 
 //Next Problem
@@ -44,7 +59,14 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 //Write a function named evenFinder that is given nums as it's only argument and removes all values that aren't even from the given array.
 
   //Code Here
-
+function evenFinder (nums){
+  var even = [];
+  for (var i =0; i < nums.length; i++){
+    if (nums[i] %2 === 0){
+      even.push(nums[i]);
+    }
+  }return even;
+}
 
 
 
